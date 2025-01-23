1fd09c5608d6ecd7206a73ed1d0700b96d58046b
MOUNAH HONORE Cesaire <cesaire.mounah-honore@inria.fr>
2025-01-23 21:05:51 +0100

    update setup scirpt

--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ rm -rf /usr/local/go && tar -C /usr/local -xzf go1.23.1.linux-amd64.tar.gz
 export PATH=$PATH:/usr/local/go/bin
 rm -f go1.23.1.linux-amd64.tar.gz
 
-# Install sysstat
-sudo apt install -y sysstat
+# Install dependencies
+sudo apt install -y sysstat make
