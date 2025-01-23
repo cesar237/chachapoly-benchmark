18e104489275dd7af5f62ed7555104f31c46d390
MOUNAH HONORE Cesaire <cesaire.mounah-honore@inria.fr>
2025-01-23 15:25:01 +0100

    add sysstat measurement

--- launch_eval.sh
+++ launch_eval.sh
@@ -32,6 +32,7 @@ for run in `seq 1 $runs`; do
     printf "%3d/%3d\r" $curr $total
     echo -n "$run;" >> $res_csv
     ./chachapoly-bench $test $msg_len $worker $pps $duration >> $res_csv &
+    sar -A -o sar-$test-$msg_len-$worker-$pps-$duration-$run.data 1 $((duration+2)) > /dev/null
     curr=$(( curr + 1 ))
 done
 done
