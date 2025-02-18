50d0b2f9ab3502a8b28bb3349c412d12f7a6cc3f
MOUNAH HONORE Cesaire <cesaire.mounah-honore@inria.fr>
2025-02-18 10:32:13 +0100

    update run-bench.sh in google bench

--- google_bench/run-bench.sh
+++ google_bench/run-bench.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/bash
 
-g++ -DMSG_LEN=$1 bench.cc -std=c++11 -lbenchmark -lpthread -lssl -lcrypto -o bench_cc
+g++ -DMSG_LEN=$1 bench.cc -lbenchmark -lpthread -lssl -lcrypto -o bench_cc
 export BENCHMARK_OUT_FORMAT=csv
 
 ./bench_cc --benchmark_out_format=csv > res.out
